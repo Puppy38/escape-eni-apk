ea370d4bd4d1341a4c9378e9d02630998893cb3a
Puppy38 <puppyspark38@gmail.com>
2025-03-29 16:07:40 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('game-cache').then((cache) => {
+      return cache.addAll([
+        'index.html',
+        'style.css',    // Add your CSS file(s) here
+        'script.js',    // Add your JS file(s) here
+        'icon.png',     // Your icon
+        'icon-512.png', // Your icon
+        // Add other assets as needed
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
